bfd841e5e5ca116eed2c1fcc63f717f85d06ed9a
chamois1 <nrabicenko5@gmail.com>
2023-11-06 20:54:44 +0200

    Main page, search field

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <input 
+        type="text" 
+        placeholder="keyword"
+      />
+      <button><i class="fas fa-search"></i>Search</button>    
+    </div>
+  );
+}
+
+export default App;
